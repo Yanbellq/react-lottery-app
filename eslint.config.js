04fd27ad1ec9a854c955abf0b22cad9ce8b75f04
Yanbellq <yanbellq@gmail.com>
2025-11-16 20:52:44 +0200

    Initialize project

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { tsx: true, jsx: true },
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            'no-unused-vars': ['error', { varsIngnorePattern: '^[A-Z_]' }],
+        },
     },
-  },
-])
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'), // @ теперь ссылается на src
+            '@components': path.resolve(__dirname, './src/components'), // @components теперь ссылается на src/components
+            '@styles': path.resolve(__dirname, './src/styles'), // @styles теперь ссылается на src/styles
+            '@hooks': path.resolve(__dirname, './src/hooks'), // @hooks теперь ссылается на src/hooks
+            '@utils': path.resolve(__dirname, './src/utils'), // @utils теперь ссылается на src/utils
+            '@services': path.resolve(__dirname, './src/services'), // @services теперь ссылается на src/services
+            '@assets': path.resolve(__dirname, './src/assets'), // @assets теперь ссылается на src/assets
+        },
+    },
+});
